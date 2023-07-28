2c655d27e7a0d92ee79f8fc5e0b73432946c307e
aryark <aryark3112@gmail.com>
2023-07-28 14:50:07 +0530

    first commit

---
+++ SubjectTeacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Phase1Section5._8
+{
+    public class SubjectTeacher : Teacher
+    {
+        private SubjectTeacher whichSubject;
+
+        public SubjectTeacher WhichSubject
+        {
+            get { return whichSubject; }
+            set { whichSubject = value; }
+        }
+    }
+}
+
